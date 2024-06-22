bdd5ce0b37caf5abb58092c69039f784d4c6a4bb
scr6w <playerzone06@gmail.com>
2024-06-22 17:39:58 -0300

    feat: bin scripts and tsconfigs transpiler

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,40 @@
+#!/usr/bin/env node
 import { Command } from 'commander';
 import Parser from "./core/parser";
 import { createGlobalEnv } from "./runtime/environment";
 import { evaluate } from "./runtime/interpreter";
 import fs from 'fs';
-import util from 'util';
+import * as path from 'path';
+
 
 const program = new Command();
+const supportedExtensions = ['.cat', '.cats'];
+const version = '0.1.0';
+
+program
+  .version(version)
+  .description('CatScript Interpreter')
+  .argument('<file>', 'CatScript file to execute')
+  .action((file) => {
+    const ext = path.extname(file);
+    if (!supportedExtensions.includes(ext)) {
+      console.error(`Unsupported file extension: ${ext}. Use ${supportedExtensions.join(', ')}`);
+      process.exit(1);
+    }
+    const filePath = path.resolve(process.cwd(), file);
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(`Could not read file: ${file}`);
+        process.exit(1);
+      }
+      const parser = new Parser();
+      const env = createGlobalEnv();
+
+      const program = parser.produceAST(data);
+
+      const result = evaluate(program, env);
+
+    });
+  });
+
+program.parse(process.argv);
